d081b6540a825b70daa631de2dad92c9d3bd4e46
dark1922 <jvdematos004@gmail.com>
2021-06-30 00:32:11 -0300

    feath: delete

--- springboot-rest-api-sample/src/main/java/br/com/springboot/controllers/GreetingsController.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/controllers/GreetingsController.java
@@ -5,10 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,4 +53,25 @@ public class GreetingsController {
 		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
 	}
 	
+	@PostMapping(value = "salvar") //mapeia a url
+	@ResponseBody //descrição  da resposta
+	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) { //recebe os dados para salvar
+		
+		//salvar o usuario save
+		Usuario user = usuarioRepository.save(usuario); //pode retorna esse usuario salvo user
+		
+		//retorna new com o response passando o user salvo e created pq vai criar o usuarioq
+		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping(value = "delete") //mapeia a url
+	@ResponseBody //descrição  da resposta
+	public ResponseEntity<String> delete(@RequestParam Long iduser) { //recebe os dados para salvar
+		
+		//qnd deleta n instacia pra outro atributo pq n tem retorno
+		usuarioRepository.deleteById(iduser); // deleta pelo id long
+		
+		//retorna new com o response passando o user salvo e created pq vai criar o usuarioq
+		return new ResponseEntity<String>("User deletado com sucesso", HttpStatus.OK);
+	}
 }
